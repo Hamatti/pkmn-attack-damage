aa335af384711c73c55154488d61f3594470da86
Juha-Matti Santala <juhamattisantala@gmail.com>
2018-11-03 23:15:40 +0200

    Create first POC

--- readme.md
+++ readme.md
@@ -3,3 +3,13 @@
 Hitting the correct numbers with attacks is crucial in Pokemon TCG. This quick-and-dirty app was built fo AKD999 to have a simple UI for finding correct cards.
 
 It's a HTML/CSS/Javascript app so there's no build process, no deployment, just open up index.html and go.
+
+## Please note:
+
+As this is a quick and dirty draft, I'll add features slowly.
+
+Things missing or not working exactly as should:
+
+- Filter by set / format
+- Filter by other fields than **Damage**
+- Pagination (right now just returns 100 first cards)
---
+++ search.js
@@ -0,0 +1,155 @@
+var button = document.getElementById("submit");
+
+function costMap(cost) {
+  var _map = {
+    Grass: "[G]",
+    Fire: "[R]",
+    Water: "[W]",
+    Lightning: "[L]",
+    Fighting: "[F]",
+    Psychic: "[P]",
+    Colorless: "[C]",
+    Darkness: "[D]",
+    Metal: "[M]",
+    Fairy: "[Y]"
+  };
+
+  return _map[cost];
+}
+
+var STANDARD_FORMAT_SETS = [
+  "sm1",
+  "smp",
+  "sm2",
+  "sm3",
+  "sm35",
+  "sm4",
+  "sm5",
+  "sm6",
+  "sm7",
+  "sm75",
+  "sm8"
+].join(",");
+
+var EXPANDED_FORMAT_SETS = [
+  "bwp",
+  "bw1",
+  "bw2",
+  "bw3",
+  "bw4",
+  "bw5",
+  "bw6",
+  "dv1",
+  "bw7",
+  "bw8",
+  "bw9",
+  "bw10",
+  "xyp",
+  "bw11",
+  "xy0",
+  "xy1",
+  "xy2",
+  "xy3",
+  "xy4",
+  "xy5",
+  "dc1",
+  "xy6",
+  "xy7",
+  "xy8",
+  "xy9",
+  "g1",
+  "xy10",
+  "xy11",
+  "xy12",
+  "smp",
+  "sm1",
+  "sm2",
+  "sm3",
+  "sm35",
+  "sm4",
+  "sm5",
+  "sm6",
+  "sm7",
+  "sm75",
+  "sm8"
+].join(",");
+
+button.addEventListener("click", function(ev) {
+  ev.preventDefault();
+  var damageValue = document.getElementById("damage").value;
+  var standard = document.getElementById("standard").checked;
+  var expanded = document.getElementById("expanded").checked;
+
+  if (!standard || !expanded) {
+    standard = true;
+  }
+
+  var standardLegalQuery = `&standardLegal=${standard}`;
+  var expandedLegalQuery = `&expandedLegal=${expanded}`;
+
+  var request = new Request(
+    `https://api.pokemontcg.io/v1/cards?attackDamage=${damageValue}${standardLegalQuery}${expandedLegalQuery}`,
+    {
+      headers: new Headers({
+        "Page-Size": 1000
+      })
+    }
+  );
+
+  fetch(request)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let table = document.createElement("table");
+      table.className = "table";
+      let thead = document.createElement("thead");
+      table.appendChild(thead);
+
+      let headers = ["Pokemon / Card", "Name", "Cost", "Description"];
+      let tr = document.createElement("tr");
+      headers.map(function(header) {
+        let th = document.createElement("th");
+        th.appendChild(document.createTextNode(header));
+        tr.appendChild(th);
+      });
+      thead.appendChild(tr);
+
+      let tbody = document.createElement("tbody");
+      table.appendChild(tbody);
+
+      data.cards.map(function(card) {
+        let tr = document.createElement("tr");
+        let id = document.createElement("td");
+        id.appendChild(document.createTextNode(`${card.name} (${card.id})`));
+
+        let attack = card.attacks.filter(function(attack) {
+          return attack.damage.startsWith(damageValue);
+        })[0];
+
+        tr.appendChild(id);
+        if (attack) {
+          let attackName = document.createElement("td");
+          attackName.appendChild(document.createTextNode(attack.name));
+
+          let attackCost = document.createElement("td");
+          let costString = "";
+          let costs = attack.cost.map(function(cost) {
+            return costMap(cost);
+          });
+          attackCost.appendChild(document.createTextNode(costs.join("")));
+
+          let attackDesc = document.createElement("td");
+          attackDesc.appendChild(document.createTextNode(attack.text));
+
+          tr.appendChild(attackName);
+          tr.appendChild(attackCost);
+          tr.appendChild(attackDesc);
+        }
+
+        tbody.appendChild(tr);
+      });
+
+      document.getElementById("results").appendChild(table);
+    });
+});
