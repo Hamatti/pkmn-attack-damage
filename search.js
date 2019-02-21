4891b1d306d1ea43f163dbaedd896642c110f1e6
Juha-Matti Santala <juhamattisantala@gmail.com>
2019-02-21 23:55:18 +0200

    Refactor DOM building into template strings

--- search.js
+++ search.js
@@ -1,64 +1,60 @@
+const STANDARD_FORMAT_SETS = [ "sm1", "smp", "sm2", "sm3", "sm35", "sm4", "sm5", "sm6", "sm7", "sm75", "sm8" ];
+const EXPANDED_FORMAT_SETS = [ "bwp", "bw1", "bw2", "bw3", "bw4", "bw5", "bw6", "dv1", "bw7", "bw8", "bw9", "bw10", "xyp", "bw11", "xy0", "xy1", "xy2", "xy3", "xy4", "xy5", "dc1", "xy6", "xy7", "xy8", "xy9", "g1", "xy10", "xy11", "xy12", ...STANDARD_FORMAT_SETS ];
+const STANDARD_FORMAT_SETS_STRING = STANDARD_FORMAT_SETS.join("|");
+const EXPANDED_FORMAT_SETS_STRING = EXPANDED_FORMAT_SETS.join("|");
+
+/**
+ * Allows sorting of an array of objects by a key
+ */
 function dynamicSort(property) {
   return function(a, b) {
     return a[property].localeCompare(b[property]);
   };
 }
 
-function costMap(cost) {
-  const node = document.createElement("img");
-  node.src = `assets/${cost.toLowerCase()}.png`;
-  return node;
+/**
+ * Generates an img tag string for an image of Pokemon type
+ */
+function getTypeImage(type) {
+  return `<img src="${'assets/' + type.toLowerCase()}.png" />`;
 }
 
-const STANDARD_FORMAT_SETS = [
-  "sm1",
-  "smp",
-  "sm2",
-  "sm3",
-  "sm35",
-  "sm4",
-  "sm5",
-  "sm6",
-  "sm7",
-  "sm75",
-  "sm8"
-];
+/**
+ * Get contents for Pokemon's types, including hidden values that are used for sorting
+ */
+function getPokemonTypes(types) {
+  return types.map(type => {
+    const hiddenType = `<span class="hidden">${type.toLowerCase()}</span>`
+    const typeElement = getTypeImage(type);
+    return `${hiddenType}${typeElement}`
+  }).join('')
+}
 
-const EXPANDED_FORMAT_SETS = [
-  "bwp",
-  "bw1_STRING",
-  "bw2",
-  "bw3",
-  "bw4",
-  "bw5",
-  "bw6",
-  "dv1",
-  "bw7",
-  "bw8",
-  "bw9",
-  "bw10",
-  "xyp",
-  "bw11",
-  "xy0",
-  "xy1",
-  "xy2",
-  "xy3",
-  "xy4",
-  "xy5",
-  "dc1",
-  "xy6",
-  "xy7",
-  "xy8",
-  "xy9",
-  "g1",
-  "xy10",
-  "xy11",
-  "xy12",
-  ...STANDARD_FORMAT_SETS
-];
+/**
+ * Build a DOM string for the rows of cards
+ */
+function getCardsDOM(cards, damageValue) {
+  return `${cards.map(card => {
+    let attack = card.attacks.filter(function(attack) {
+      return attack.damage.startsWith(damageValue);
+    })[0];
 
-const STANDARD_FORMAT_SETS_STRING = STANDARD_FORMAT_SETS.join("|");
-const EXPANDED_FORMAT_SETS_STRING = EXPANDED_FORMAT_SETS.join("|");
+    if(attack) {
+      return `<tr>
+          <td>${card.name} (${card.id})</td>
+          <td>${attack.name}</td>
+          <td>${getPokemonTypes(card.types)}</td>
+          <td>${attack.damage}</td>
+          <td>${attack.cost.map(cost => getTypeImage(cost)).join('')}</td>
+          <td>${attack.text}</td>
+        </tr>
+        `
+    }
+  }
+  ).join('')}`
+}
+
+//------ Main logic ------------//
 var button = document.getElementById("submit");
 
 button.addEventListener("click", function(ev) {
@@ -89,9 +85,6 @@ button.addEventListener("click", function(ev) {
       return response.json();
     })
     .then(function(data) {
-      // Remove old result
-      document.getElementById("results").innerHTML = "";
-
       var cards = data.cards;
       cards.sort(dynamicSort("name"));
 
@@ -101,12 +94,7 @@ button.addEventListener("click", function(ev) {
         );
       }
 
-      let table = document.createElement("table");
-      table.className = "table sortable";
-      let thead = document.createElement("thead");
-      table.appendChild(thead);
-
-      let headers = [
+      const headers = [
         "Pokemon / Card",
         "Name",
         "Type",
@@ -114,67 +102,21 @@ button.addEventListener("click", function(ev) {
         "Cost",
         "Description"
       ];
-      let tr = document.createElement("tr");
-      headers.map(function(header) {
-        let th = document.createElement("th");
-        th.appendChild(document.createTextNode(header));
-        tr.appendChild(th);
-      });
-      thead.appendChild(tr);
 
-      let tbody = document.createElement("tbody");
-      table.appendChild(tbody);
+      const table = `
+        <table class="table sortable">
+          <thead>
+            <tr>
+            ${headers.map(header => `<th>${header}</th>`).join('')}
+            </tr>
+          </thead>
+          <tbody>
+            ${getCardsDOM(cards, damageValue)}
+          </tbody>
+        </table>`;
 
-      cards.map(function(card) {
-        let tr = document.createElement("tr");
-        let id = document.createElement("td");
-        id.appendChild(document.createTextNode(`${card.name} (${card.id})`));
+      document.getElementById("results").innerHTML = table;
 
-        let attack = card.attacks.filter(function(attack) {
-          return attack.damage.startsWith(damageValue);
-        })[0];
-
-        tr.appendChild(id);
-        if (attack) {
-          let attackName = document.createElement("td");
-          attackName.appendChild(document.createTextNode(attack.name));
-
-          let attackCost = document.createElement("td");
-          let costString = "";
-          attack.cost.forEach(function(cost) {
-            attackCost.appendChild(costMap(cost));
-          });
-
-          let attackDesc = document.createElement("td");
-          attackDesc.appendChild(document.createTextNode(attack.text));
-
-          let attackDamage = document.createElement("td");
-          attackDamage.appendChild(document.createTextNode(attack.damage));
-
-          let pokemonType = document.createElement("td");
-          card.types.forEach(function(type) {
-            const child = costMap(type);
-            const hidden_type = document.createElement("span");
-            hidden_type.appendChild(
-              document.createTextNode(type.toLowerCase())
-            );
-            hidden_type.className = "hidden";
-            pokemonType.appendChild(hidden_type);
-            child.setAttribute("sorttable_customkey", type);
-            pokemonType.appendChild(child);
-          });
-
-          tr.appendChild(attackName);
-          tr.appendChild(pokemonType);
-          tr.appendChild(attackDamage);
-          tr.appendChild(attackCost);
-          tr.appendChild(attackDesc);
-        }
-
-        tbody.appendChild(tr);
-      });
-
-      document.getElementById("results").appendChild(table);
-      sorttable.makeSortable(table);
+      sorttable.makeSortable(document.getElementsByTagName('table')[0]);
     });
 });
